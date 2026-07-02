1206c6d6dbebeabfebec7e3b36a4e485b01433d8
johaoooo <josephdehazounde@gmail.com>
2026-07-02 23:05:12 +0100

    🐛 Fix: Configuration des fichiers statiques pour Render

--- afi_backend/urls.py
+++ afi_backend/urls.py
@@ -2,12 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
 ]
 
-# Servir les fichiers media en développement
+# Servir les fichiers statiques en développement
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # En production, servir les fichiers statiques
+    urlpatterns += staticfiles_urlpatterns()
